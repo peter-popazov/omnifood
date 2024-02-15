2ddcf50ae98713f5248c8d1b9507c3b339867627
Peter <10252129@stud.op.edu.ua>
2024-02-15 15:35:38 +0200

    js smooth scrolling added

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
+// Setting current year in copyright section
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Open nav for mobile devices
 const header = document.querySelector(".header");
-
 const btnMobileMenu = document.querySelector(".btn-mobile-nav");
-
 btnMobileMenu.addEventListener("click", function () {
   header.classList.toggle("nav-open");
+});
+
+// Smooth scrolling
+const allLinks = document.querySelectorAll("a");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      if (link.classList.contains("main-nav-link")) {
+        header.classList.remove("nav-open");
+      }
+    }
+  });
 });
